371ea06ae06c40e253223972af07320d037c6021
Igor Baksht <igorexa81@gmail.com>
2019-07-29 23:16:10 -0600

    Updates

---
+++ precache-manifest.e95d58a3f62d04d81d0cb8762ffa9442.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0321f18a52106c4ac8886e33fcfd5a",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "ccfd36ea3c50cdd8ff97",
+    "url": "/clicky_game/static/css/main.e88de197.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky_game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ccfd36ea3c50cdd8ff97",
+    "url": "/clicky_game/static/js/main.41dd6259.chunk.js"
+  },
+  {
+    "revision": "ef6ad64b0df3315f3b6a",
+    "url": "/clicky_game/static/js/runtime~main.33627756.js"
+  }
+]);
